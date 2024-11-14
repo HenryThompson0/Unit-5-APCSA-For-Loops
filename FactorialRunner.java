8cb46df2427bb6c292b440a866f54bf31a3af898
HenryThompson0 <sayjokesphotos@gmail.com>
2024-11-14 07:29:24 -0500

    Update FactorialRunner.java

--- FactorialRunner.java
+++ FactorialRunner.java
@@ -2,9 +2,18 @@
 //www.apluscompsci.com
 //Name
 
+import java.util.Scanner;
+
 public class FactorialRunner
 {
 	public static void main ( String[] args )
 	{
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Enter your number: ");
+
+        int choice = kb.nextInt();
+        System.out.println(Factorial.getFactorial(choice));
+
+        kb.close();
 	}
 }
