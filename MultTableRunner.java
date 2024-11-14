56167432b18235bb5242e84e48f382fcbc2c3c76
HenryThompson0 <sayjokesphotos@gmail.com>
2024-11-14 07:25:40 -0500

    Update MultTableRunner.java

--- MultTableRunner.java
+++ MultTableRunner.java
@@ -1,12 +1,22 @@
+package Assigned;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name
+
+import java.util.Scanner;
 
 public class MultTableRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases
-		
+		Scanner kb = new Scanner(System.in);
+
+		System.out.print("What number to multiply with? ");
+		int multiplier = kb.nextInt();
+
+		System.out.print("How many times to multiply? ");
+		int amount = kb.nextInt();
+
+		MultTable n = new MultTable(multiplier, amount);
 	}
 }
