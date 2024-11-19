8cf5d681d11ec07da4dbf4e87caf3bb0eb88d880
HenryThompson0 <sayjokesphotos@gmail.com>
2024-11-19 09:04:19 -0500

    Update TriangleWord.java

--- TriangleWord.java
+++ TriangleWord.java
@@ -1,3 +1,4 @@
+package NewStuff;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
@@ -9,6 +10,27 @@ public class TriangleWord
 	public TriangleWord(String w)
 	{
 		word=w;
+
+		System.out.println("   "+word.charAt(0));
+		System.out.println(" "+word.charAt(1)+"  "+word.charAt(1));
+		System.out.print(reverse());
+		System.out.print(word.substring(1,word.length()));
+
+
+	}
+
+	public String reverse()
+	{
+		String reversed = "";
+		String newWord = word;
+
+		while (newWord.length()>0)
+		{
+			reversed += newWord.charAt(newWord.length()-1);
+			newWord = newWord.substring(0, newWord.length()-1);
+		}
+
+		return reversed;
 	}
 
 	public String toString()
