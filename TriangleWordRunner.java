32b061acca0df3cba5563074e46f8c0fdf5d187d
HenryThompson0 <sayjokesphotos@gmail.com>
2024-11-19 09:04:07 -0500

    Update TriangleWordRunner.java

--- TriangleWordRunner.java
+++ TriangleWordRunner.java
@@ -1,10 +1,18 @@
+package NewStuff;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
 
+import java.util.Scanner;
+
 public class TriangleWordRunner
 {
    public static void main(String args[])
-   {	
+   {
+      Scanner kb = new Scanner(System.in);
+      System.out.print("Enter your word: ");
+      String word = kb.nextLine();
+
+      TriangleWord k = new TriangleWord(word);
 	}
 }
